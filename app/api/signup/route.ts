cd23c9151c77783a7326d343d46aa8df65502163
Aswin <117593557+AswinM1@users.noreply.github.com>
2026-05-31 16:57:18 +0530

    ui

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,10 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
-import { connector } from "@/app/lib/dbConnect";
 import { prisma } from "@/app/lib/prisma";
 
-
-connector()
 export async function POST(req:NextRequest){
  
     const body=await req.json()

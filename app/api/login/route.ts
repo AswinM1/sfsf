b364e627f8cf53356f86de52373d3b90646c0240
Aswin <117593557+AswinM1@users.noreply.github.com>
2026-05-31 19:04:54 +0530

    sa

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -2,37 +2,48 @@ import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 import { prisma } from "../../lib/prisma";
 
-export async function POST(req:NextRequest){
-    const body=await req.json()
-    const {email,password}=body
-     const user=await prisma.user.create({data:{email:email,password:password}});
-     if(user)
-     {
-      const tokendata={
-         
-         userId:user.id
-         
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+
+    const { email, password } = body;
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password,
+      },
+    });
+
+    const tokendata = {
+      userId: user.id,
+    };
+
+    const token = jwt.sign(
+      tokendata,
+      process.env.SECRET_KEY!
+    );
+
+    const response = NextResponse.json({
+      message: "login success",
+      token,
+      success: true,
+    });
+
+    response.cookies.set("token", token);
+
+    return response;
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        message: "server error",
+        success: false,
+      },
+      {
+        status: 500,
       }
-        const token=jwt.sign(tokendata,process.env.secret_key!)
-
-       
-        const response=NextResponse.json({
-         message:"login success",
-         token:token,
-         success:true
-        })
-        response.cookies.set("token",token)
-
-        return response
-     }
-     else
-     {
-        return NextResponse.json({
-           message:"not found",
-           success:false
-        })
-     }
-
-
-    
+    );
+  }
 }
